b5316193b426edcf683ed062beb7e6c4fbfd3d5e
Bunyamin Arslan <110098812+ArslanBunyamin@users.noreply.github.com>
2024-06-06 23:09:20 +0300

    Update layout.tsx for bug fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
       <StoreProvider>
         <body className={inter.className}>
           <AppRouterCacheProvider>
-            <div className="h-screen">
+            <div className="min-h-screen">
               <Navbar />
               <div className="pt-16 h-full">{children}</div>
               <Footer />
